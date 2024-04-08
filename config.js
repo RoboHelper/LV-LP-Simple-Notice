a908a251da94438939fa4ae776ff06539be86efc
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-04-08 11:39:15 +0300

    G tag addition

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
-const domainName = "helpforallusa.com";
+const domainName = "helpedeveryone.com";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
+const G2DisplayName = "";
 const G2Link =
   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
 
 const addLeadId = true;
 
-const qualifyNumber = "8555792872";
-const overflowNumber = "8556714382";
+const qualifyNumber = "8776595463";
+const overflowNumber = "8776512362";
 
 const CustomText =
-  "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
+  "All Americans Will Receive $6,100 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "";
-const googleConversionTagEventSentTo = "";
+const googleAccountConversionTagID = "AW-16519928028";
+const googleConversionTagEventSentTo = "AW-16519928028/qG8gCOiwjKMZENyxqMU9";
 const googleConversionTagValue = 1.0;
 
 const qualifyDebtNumber = "";
