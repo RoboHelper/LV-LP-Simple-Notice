59561a962c6d9e5379522aa161fbee855dce74fb
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-04-09 21:22:49 +0300

    Fixed configs

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-const domainName = "helpforallusa.com";
+const domainName = "usacaguru.com";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
-const G2Link =
-  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
+const G2DisplayName = "";
+// ("Senior Care Direct");
+const G2Link = "";
+// "https://g2risksolutions.com/approved-health-insurance-provider/?pdb=2008";
 
 const addLeadId = true;
 
-const qualifyNumber = "8555792872";
-const overflowNumber = "8556714382";
+const qualifyNumber = "+18887103121";
+const overflowNumber = "+18336610141";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
@@ -16,16 +17,16 @@ const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "";
-const googleConversionTagEventSentTo = "";
+const googleAccountConversionTagID = "AW-16472031230";
+const googleConversionTagEventSentTo = "AW-16472031230/5SAxCN6a3psZEP7_vK49";
 const googleConversionTagValue = 1.0;
 
-const qualifyDebtNumber = "";
+const qualifyDebtNumber = "+18313069041";
 const normalRingbaDebtCampaignID = "CA3d44f20051764969b1181db77bfbfd7b";
-const overflowDebtNumber = "";
+const overflowDebtNumber = "+17088520719";
 const overflowRingbaDebtCampaignID = "CA6ba3ea908b3149afb82e3a3904113a08";
 
-const qualifySpDebtNumber = "";
+const qualifySpDebtNumber = "+18774950176";
 const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
-const overflowSpDebtNumber = "";
+const overflowSpDebtNumber = "+18556013376";
 const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
