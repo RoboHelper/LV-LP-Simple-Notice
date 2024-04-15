f8eee492c5435631764a05f83a63d7c547709abd
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-04-16 00:47:04 +0500

    dar

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const domainName = "helpforallusa.com";
+const domainName = "helpinghealthstation.com";
 
 const G2DisplayName = "Peak Plans LLC. - G2 Certification";
 const G2Link =
@@ -6,8 +6,8 @@ const G2Link =
 
 const addLeadId = true;
 
-const qualifyNumber = "8555792872";
-const overflowNumber = "8556714382";
+const qualifyNumber = "+18774015410";
+const overflowNumber = "+18339962167";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
