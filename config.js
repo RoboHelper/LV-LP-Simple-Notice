ea7dc1e7d745c9a8b7d54a2c968173d2be2a6c1b
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-04-18 23:33:26 +0500

    dz done

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const domainName = "helpforallusa.com";
+const domainName = "helpinggoodhumans.com";
 
 const G2DisplayName = "Peak Plans LLC. - G2 Certification";
 const G2Link =
@@ -6,8 +6,8 @@ const G2Link =
 
 const addLeadId = true;
 
-const qualifyNumber = "8555792872";
-const overflowNumber = "8556714382";
+const qualifyNumber = "8668846454";
+const overflowNumber = "8667600926";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
@@ -16,8 +16,8 @@ const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "";
-const googleConversionTagEventSentTo = "";
+const googleAccountConversionTagID = "AW-16498959505";
+const googleConversionTagEventSentTo = "AW-16498959505/y_dMCMOP3Z4ZEJHJqLs9";
 const googleConversionTagValue = 1.0;
 
 const qualifyDebtNumber = "";
