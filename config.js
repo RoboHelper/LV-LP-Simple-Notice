67c9eaecc9e059c49567d6cb7ddc709be631d712
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-04-29 18:20:43 +0300

    G Tag

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "";
-const googleConversionTagEventSentTo = "";
+const googleAccountConversionTagID = "AW-16526243908";
+const googleConversionTagEventSentTo = "AW-16526243908/0cLnCPuZnasZEMTwqcg9";
 const googleConversionTagValue = 1.0;
 
 const qualifyDebtNumber = "";
