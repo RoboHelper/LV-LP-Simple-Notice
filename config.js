5e88b872202819e359f89699da9a27221b1ab803
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-05-03 02:20:06 +0500

    comp

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
-const domainName = "helpforallusa.com";
+const domainName = "helpeveryonealways.com";
 
 const G2DisplayName = "Peak Plans LLC. - G2 Certification";
 const G2Link =
-  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
+   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
 
 const addLeadId = true;
 
-const qualifyNumber = "8555792872";
-const overflowNumber = "8556714382";
-
-const CustomText =
-  "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
-const CustomTextCall = "You're Pre-Qualified for Subsidy!";
+const qualifyNumber = "8312454134";
+const overflowNumber = "7257052312";
 
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "";
-const googleConversionTagEventSentTo = "";
+const googleAccountConversionTagID = "AW-11486229909";
+const googleConversionTagEventSentTo = "AW-11486229909/f_N7CLqNh5AZEJXrh-Uq";
 const googleConversionTagValue = 1.0;
+
+const CustomText =
+  "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
+const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
 const qualifyDebtNumber = "";
 const normalRingbaDebtCampaignID = "CA3d44f20051764969b1181db77bfbfd7b";
 const overflowDebtNumber = "";
 const overflowRingbaDebtCampaignID = "CA6ba3ea908b3149afb82e3a3904113a08";
 
-const qualifySpDebtNumber = "";
+const qualifySpDebtNumber = "+18774170359";
 const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
 const overflowSpDebtNumber = "";
 const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
