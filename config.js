6ca9766becadc5b72e64b8723afc2decf23d0963
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-05-14 04:18:40 +0500

    done

--- config.js
+++ config.js
@@ -28,5 +28,5 @@ const overflowRingbaDebtCampaignID = "CA6ba3ea908b3149afb82e3a3904113a08";
 
 const qualifySpDebtNumber = "+17732490378";
 const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
-const overflowSpDebtNumber = "";
+const overflowSpDebtNumber = "+18446168932";
 const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
