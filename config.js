12daa83850ca183f7e3d7acc7226ed28569e0b03
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-05-14 19:46:51 +0300

    Correct

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const domainName = "helpsforeveryone.com";
 
-const G2DisplayName = "Quick Health Inc. - G2 Certification";
+const G2DisplayName = "";
 const G2Link =
   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=108";
 
 const addLeadId = true;
 
 const qualifyNumber = "8665500208";
-// const overflowNumber = "8446731187";
-const overflowNumber = "8665500208";
+const overflowNumber = "8446731187";
+// const overflowNumber = "8665500208";
 
 const CustomText =
   "All Americans Will Receive $6,100 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
-const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
-// const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+// const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
+const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
 const googleAccountConversionTagID = "AW-16453975244";
