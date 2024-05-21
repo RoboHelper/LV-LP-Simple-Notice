79bc484dd280afd9526872605a4679085ae33830
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-05-21 15:20:21 +0300

    Correct configs

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const domainName = "usacaguru.com";
+const domainName = "helpingforyou.com";
 
-const G2DisplayName = "Senior Care Direct";
+const G2DisplayName = "";
 const G2Link =
   "https://g2risksolutions.com/approved-health-insurance-provider/?pdb=2008";
 
 const addLeadId = true;
 
-const qualifyNumber = "+18887103121";
-const overflowNumber = "+18336610141";
+const qualifyNumber = "+18556752852";
+const overflowNumber = "+18445412437";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
@@ -16,16 +16,16 @@ const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "AW-16472031230";
-const googleConversionTagEventSentTo = "AW-16472031230/5SAxCN6a3psZEP7_vK49";
+const googleAccountConversionTagID = "AW-16473800537";
+const googleConversionTagEventSentTo = "AW-16473800537/_1bACKLW_qAZENn-qK89";
 const googleConversionTagValue = 1.0;
 
-const qualifyDebtNumber = "+18313069041";
-const normalRingbaDebtCampaignID = "CA3d44f20051764969b1181db77bfbfd7b";
-const overflowDebtNumber = "+17088520719";
-const overflowRingbaDebtCampaignID = "CA6ba3ea908b3149afb82e3a3904113a08";
+const qualifyDebtNumber = "";
+const normalRingbaDebtCampaignID = "";
+const overflowDebtNumber = "";
+const overflowRingbaDebtCampaignID = "";
 
-const qualifySpDebtNumber = "+18774950176";
-const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
-const overflowSpDebtNumber = "+18556013376";
-const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
+const qualifySpDebtNumber = "";
+const normalRingbaSpDebtCampaignID = "";
+const overflowSpDebtNumber = "";
+const overflowRingbaSpDebtCampaignID = "";
