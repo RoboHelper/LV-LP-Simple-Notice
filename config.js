7483e9f1460cdf825ca83aa7ddda7c51d22b8ce8
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-05-24 00:14:23 +0300

    Correct configs

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const domainName = "usreliefstation.com";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
+const G2DisplayName = "";
 const G2Link =
   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
 
 const addLeadId = true;
 
 const qualifyNumber = "8333730233";
-const overflowNumber = "8333730233";
-// const overflowNumber = "8336701580";
+// const overflowNumber = "8333730233";
+const overflowNumber = "8336701580";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
-// const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
-const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
+const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+// const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
 const googleAccountConversionTagID = "AW-11483400288";
