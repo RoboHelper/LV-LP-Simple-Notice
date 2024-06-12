2a194272296fe5068511aca2d0fe3eb108e52071
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-06-13 02:53:27 +0500

    done

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const G2Link =
 
 const addLeadId = true;
 
-const qualifyNumber = "+18887103121";
-const overflowNumber = "+18336610141";
+const qualifyNumber = "+17252521751";
+const overflowNumber = "+18776481525";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
@@ -16,16 +16,16 @@ const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "AW-16472031230";
-const googleConversionTagEventSentTo = "AW-16472031230/5SAxCN6a3psZEP7_vK49";
+const googleAccountConversionTagID = "AW-16510766701";
+const googleConversionTagEventSentTo = "AW-16510766701/L7dlCIzajaMZEO2c-cA9";
 const googleConversionTagValue = 1.0;
 
-const qualifyDebtNumber = "+18313069041";
+const qualifyDebtNumber = "";
 const normalRingbaDebtCampaignID = "CA3d44f20051764969b1181db77bfbfd7b";
-const overflowDebtNumber = "+17088520719";
+const overflowDebtNumber = "";
 const overflowRingbaDebtCampaignID = "CA6ba3ea908b3149afb82e3a3904113a08";
 
-const qualifySpDebtNumber = "+18774950176";
+const qualifySpDebtNumber = "+18776598138";
 const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
-const overflowSpDebtNumber = "+18556013376";
+const overflowSpDebtNumber = "+18776591464";
 const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
