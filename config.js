8497d3b2eb3b3590f0a11f72ba592cf3ae54820d
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-07-08 22:23:03 +0300

    Temp

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const domainName = "peopleshelthcentre.com";
 
-const G2DisplayName = "Quick Health Inc. - G2 Certification";
+const G2DisplayName = "";
 const G2Link =
   "https://g2risksolutions.com/approved-health-insurance-provider/?pdb=108";
 
 const addLeadId = true;
 
 const qualifyNumber = "+18774941006";
-const overflowNumber = "+18885616321";
+const overflowNumber = "+18774941006";
+// const overflowNumber = "+18885616321";
 
 const CustomText =
   "All Americans Will Receive $5,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
-const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+// const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
 const googleAccountConversionTagID = "AW-16520486546";
