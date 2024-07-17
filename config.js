229bd3c2b1b3acffbb2496128c55e47f1cb1bf0f
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-07-18 01:46:16 +0300

    temp

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const domainName = "helpinghealthguru.com";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
+const G2DisplayName = "";
 const G2Link =
   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
 
 const addLeadId = true;
 
 const qualifyNumber = "+18337650181";
-const overflowNumber = "+18559433772";
+const overflowNumber = "+18337650181";
+// const overflowNumber = "+18559433772";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
-const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+// const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
 const googleAccountConversionTagID = "AW-16526243908";
