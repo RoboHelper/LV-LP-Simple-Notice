95cbd13d8dfec5122d6d9e98a63335bc2d3fa9e1
nemanjabbx <nemanja@bbx.rs>
2024-07-24 20:54:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -29,3 +29,5 @@ const qualifySpDebtNumber = "";
 const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
 const overflowSpDebtNumber = "";
 const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
+
+...
