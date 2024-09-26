7e2984736ae3a1edaa8dc5ef15a17489d65f9d65
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-09-26 04:05:56 +0300

    Updated Phone numbers

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const domainName = "https://guruaidrelief.com/";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
+const G2DisplayName = "";
 const G2Link =
   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
 
-const addLeadId = true;
+// const addLeadId = true;
 
-const qualifyNumber = "+18557684595";
-const overflowNumber = "+18557684595";
-// const overflowNumber = "+18888710561";
+const qualifyNumber = "+13215033514";
+// const overflowNumber = "+18557684595";
+const overflowNumber = "+18588985759";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
-// const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
-const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
+const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+// const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
 const googleAccountConversionTagID = "AW-11337646891";
