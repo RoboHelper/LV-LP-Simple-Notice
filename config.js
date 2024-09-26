8893d93d32c1af073b0c7881e383acbfbba05399
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-09-26 20:25:00 +0300

    Updated Phone numbers

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const G2Link =
 
 const addLeadId = true;
 
-const qualifyNumber = "+18312578633";
-const overflowNumber = "+13218780328";
+const qualifyNumber = "+16199277552";
+const overflowNumber = "+13232208369";
 
 const CustomText =
   "All Americans Will Receive $7,250 in Subsidies to spend on Rent, Gas, and Groceries.";
