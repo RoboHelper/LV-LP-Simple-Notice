a8385b5aed1b51e3a0197ffbab37ace0e26de857
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-09-26 04:14:22 +0300

    Updated Phone numbers

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ const G2Link =
 
 const addLeadId = true;
 
-const qualifyNumber = "+18337650181";
-const overflowNumber = "+18337650181";
-// const overflowNumber = "+18559433772";
+const qualifyNumber = "+13215590237";
+// const overflowNumber = "+18337650181";
+const overflowNumber = "+17252919985";
 
 const CustomText =
   "All Americans Will Receive $6,400 in Subsidies to spend on Rent, Gas, Groceries, or anything else.";
 const CustomTextCall = "You're Pre-Qualified for Subsidy!";
 
-// const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
-const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
+const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+// const overflowRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
 const googleAccountConversionTagID = "AW-16526243908";
