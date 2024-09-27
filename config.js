c5fcf681302b7a605be8a768a78711aead3abfc1
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-09-27 06:03:15 +0300

    Updated Phone numbers

--- config.js
+++ config.js
@@ -4,19 +4,16 @@ const G2DisplayName = "Peak Plans LLC. - G2 Certification";
 const G2Link =
   "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
 
-
-  const CustomText = `
+const CustomText = `
   Odd Exotic Coffee Loophole 
   <span>Melts Away Years of Stubborn Belly Fat In Weeks</span>
 `;
 
-
-
 const addLeadId = true;
 
-const qualifyNumber = "+16074144180";
-const overflowNumber = "+16074144180";
-const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
+const qualifyNumber = "+13802470692";
+const overflowNumber = "+13802470692";
+const overflowRingbaCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
 const normalRingbaCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
 
 const googleAccountConversionTagID = "";
@@ -32,4 +29,3 @@ const qualifySpDebtNumber = "";
 const normalRingbaSpDebtCampaignID = "CA4d1d63d6f1054fb5be8f69dc661fc681";
 const overflowSpDebtNumber = "";
 const overflowRingbaSpDebtCampaignID = "CAfcac3e7c53f94634b91cf2e55a6166f4";
-
